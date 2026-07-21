df7a7fcdc56cbdcba960e870e704137ad952779f
Mufaddal Aurangabadi <mufaddalaurangabadi@gmail.com>
2026-07-21 20:55:57 +0530

    sanity cms grouping

--- sanity/structure.js
+++ sanity/structure.js
@@ -1,100 +1,102 @@
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
+
+const singletonItem = (S, {title, schemaType, documentId = schemaType}) =>
+  S.listItem()
+    .title(title)
+    .id(documentId)
+    .child(S.document().schemaType(schemaType).documentId(documentId))
+
 export const structure = (S) =>
   S.list()
-    .title('Content')
+    .title('ARK Simplify')
     .items([
       S.listItem()
-        .title('Homepage')
-        .id('homepage')
-        .child(S.document().schemaType('homepage').documentId('homepage')),
-      S.listItem()
-        .title('SEO settings')
-        .id('seoSettings')
+        .title('SEO & site configuration')
+        .id('siteConfiguration')
         .child(
-          S.document().schemaType('seoSettings').documentId('seoSettings'),
+          S.list()
+            .title('SEO & site configuration')
+            .items([
+              singletonItem(S, {
+                title: 'SEO settings',
+                schemaType: 'seoSettings',
+              }),
+              singletonItem(S, {
+                title: 'Head code',
+                schemaType: 'headCodeSettings',
+              }),
+            ]),
         ),
+
       S.listItem()
-        .title('Head code')
-        .id('headCodeSettings')
+        .title('Website pages')
+        .id('websitePages')
         .child(
-          S.document()
-            .schemaType('headCodeSettings')
-            .documentId('headCodeSettings'),
+          S.list()
+            .title('Website pages')
+            .items([
+              singletonItem(S, {
+                title: 'Homepage',
+                schemaType: 'homepage',
+              }),
+              singletonItem(S, {
+                title: 'About us',
+                schemaType: 'aboutPage',
+              }),
+              singletonItem(S, {
+                title: 'Construction',
+                schemaType: 'constructionPage',
+              }),
+              singletonItem(S, {
+                title: 'Construction landing',
+                schemaType: 'constructionLandingPage',
+              }),
+              singletonItem(S, {
+                title: 'Finance',
+                schemaType: 'financePage',
+              }),
+              singletonItem(S, {
+                title: 'Finance landing',
+                schemaType: 'financeLandingPage',
+              }),
+              singletonItem(S, {
+                title: 'ProEstimate',
+                schemaType: 'proestimatePage',
+              }),
+              singletonItem(S, {
+                title: 'Life at ARK',
+                schemaType: 'lifeAtArkPage',
+              }),
+              singletonItem(S, {
+                title: 'Contact',
+                schemaType: 'contact',
+              }),
+            ]),
         ),
+
       S.listItem()
-        .title('Contact')
-        .id('contact')
-        .child(S.document().schemaType('contact').documentId('contact')),
-      S.listItem()
-        .title('About us page')
-        .id('aboutPage')
-        .child(S.document().schemaType('aboutPage').documentId('aboutPage')),
-      S.listItem()
-        .title('Construction page')
-        .id('constructionPage')
+        .title('Content & publishing')
+        .id('contentPublishing')
         .child(
-          S.document()
-            .schemaType('constructionPage')
-            .documentId('constructionPage'),
-        ),
-      S.listItem()
-        .title('ARK Simplify construction landing')
-        .id('constructionLandingPage')
-        .child(
-          S.document()
-            .schemaType('constructionLandingPage')
-            .documentId('constructionLandingPage'),
-        ),
-      S.listItem()
-        .title('Finance page')
-        .id('financePage')
-        .child(S.document().schemaType('financePage').documentId('financePage')),
-      S.listItem()
-        .title('ARK Simplify finance landing')
-        .id('financeLandingPage')
-        .child(
-          S.document()
-            .schemaType('financeLandingPage')
-            .documentId('financeLandingPage'),
-        ),
-      S.listItem()
-        .title('ProEstimate page')
-        .id('proestimatePage')
-        .child(
-          S.document()
-            .schemaType('proestimatePage')
-            .documentId('proestimatePage'),
-        ),
-      S.listItem()
-        .title('Life at Ark page')
-        .id('lifeAtArkPage')
-        .child(
-          S.document()
-            .schemaType('lifeAtArkPage')
-            .documentId('lifeAtArkPage'),
-        ),
-      S.listItem()
-        .title('Job openings')
-        .id('jobOpenings')
-        .child(
-          S.document().schemaType('jobOpenings').documentId('jobOpenings'),
-        ),
-      S.listItem()
-        .title('Blogs')
-        .id('blogs')
-        .child(S.documentTypeList('blog').title('Blogs')),
-      S.listItem()
-        .title('Testimonials')
-        .id('testimonials')
-        .child(
-          S.document().schemaType('testimonials').documentId('testimonials'),
-        ),
-      S.listItem()
-        .title('LinkedIn articles')
-        .id('linkedinArticles')
-        .child(
-          S.document()
-            .schemaType('linkedinArticles')
-            .documentId('linkedinArticles'),
+          S.list()
+            .title('Content & publishing')
+            .items([
+              singletonItem(S, {
+                title: 'Job openings',
+                schemaType: 'jobOpenings',
+              }),
+              S.listItem()
+                .title('Blogs')
+                .id('blogs')
+                .child(S.documentTypeList('blog').title('Blogs')),
+              singletonItem(S, {
+                title: 'LinkedIn articles',
+                schemaType: 'linkedinArticles',
+              }),
+              singletonItem(S, {
+                title: 'Testimonials',
+                schemaType: 'testimonials',
+              }),
+            ]),
         ),
     ])
